ee36f88479f14d34e0062ff626c643b2c6faa089
criztiandev <criztiandev@gmail.com>
2024-02-25 17:11:25 +0800

    Added Badge

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import Avatar from "./components/ui/Avatar";
+import Badge from "./components/ui/Badge";
 
 function App() {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <Avatar
-        src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        rounded
-      />
-
-      <Avatar src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+    <div className="">
+      <Badge>Test</Badge>
+      <Badge variant="success">Test</Badge>
+      <Badge variant="info">Test</Badge>
+      <Badge variant="warning">Test</Badge>
     </div>
   );
 }
--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -1,104 +1,49 @@
-import React from "react";
+import { HTMLAttributes, forwardRef } from "react";
 import { cn } from "../../utils/tailwind.utils";
 
-interface Props {
-  type?: "normal" | "chip" | "indicator";
-  as?: "normal" | "outline" | "ghost";
-  flag?: "success" | "error" | "warning" | "info" | "none";
-  className?: string;
-  children?: React.ReactNode;
-  rounded?: boolean;
-  dir?: "left" | "right";
-  icon?: React.ReactNode;
-  onToggle?: () => void;
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  as?: "button" | "icon" | "link";
+  variant?: "outline" | "ghost" | "info" | "success" | "warning" | "danger";
+  status?: "success" | "error" | "warning" | "info" | "none";
+  size?: "default" | "sm" | "lg" | "xl";
 }
 
-const Badge = ({ icon, dir = "left", type = "normal", ...props }: Props) => {
-  const variant = {
-    normal: "",
-    outline: "bg-transparent border border-blue-500 text-blue-500",
-    ghost: "bg-transparent text-blue-500",
-    link: "cursor-pointer underline",
-  };
+const variants = {
+  outline: "border border-input hover:bg-primary/90",
+  ghost: "bg-transparent",
+  info: "bg-blue-50 text-blue-800  hover:bg-blue-200/90 hovr:text-blue-900",
+  success:
+    "bg-green-50 text-green-800 hover:bg-green-200/90 hovr:text-green-900",
+  danger: "bg-red-50 text-red-800  hover:bg-red-200/90 hovr:text-red-900",
+  warning:
+    "bg-yellow-50 text-red-800  hover:bg-yellow-200/90 hovr:text-yellow-900",
+};
 
-  const flagColor = {
-    success:
-      "border-green-400 focus:ring-green-300 dark:focus:ring-green-400 bg-green-50",
-    error:
-      "border-red-400 focus:ring-red-300 dark:focus:ring-red-400 bg-red-50",
-    warning:
-      "border-yellow-400 focus:ring-yellow-300 dark:focus:ring-yellow-400 bg-yellow-50",
-    info: "border-blue-400 focus:ring-blue-300 dark:focus:ring-blue-400 bg-blue-50",
-    none: "",
-  };
-  const defaultStyle = cn(
-    `
-     text-xs text-black font-medium me-2 px-2.5 py-0.5 rounded w-[50px] text-center border flex items-center justify-center gap-[5px]
-    ${flagColor[props.flag || "info"]}
-    ${variant[props.as || "normal"]}
-    ${props.rounded && "rounded-full"}
-  `,
-    props.className
-  );
+const borderVariants = {
+  outline: "border border-gray-300",
+  ghost: "",
+  info: "border border-blue-300",
+  success: "border border-green-300",
+  danger: "border border-red-300",
+  warning: "border border-yellow-300",
+};
 
-  const Component = {
-    normal: (
-      <div className={defaultStyle} onClick={props.onToggle}>
-        {icon && dir === "left" && icon}
+const Badge = forwardRef<HTMLDivElement, Props>(
+  ({ variant, className, ...props }, ref) => {
+    const defaultStyle = cn(
+      `cursor-default px-2.5 py-0.5  inline-flex items-center rounded-[5px] border text-xs font-semibold
+    ${variants[variant || "outline"]}
+    ${borderVariants[variant || "outline"]}
+    `,
+      className
+    );
+
+    return (
+      <div {...props} ref={ref} className={defaultStyle}>
         {props.children}
-        {icon && dir === "right" && icon}
       </div>
-    ),
-
-    chip: <Chip {...props} />,
-    indicator: <Indicator {...props} />,
-  };
-
-  return <>{Component[type || "normal"]}</>;
-};
-
-const Chip = (props: Props) => {
-  const defaultStyle = cn(
-    `inline-flex items-center justify-between px-2 py-1 me-2 text-sm font-medium w-[70px] text-blue-800 bg-blue-100 rounded `,
-    props.className
-  );
-
-  return (
-    <Badge {...props} className={defaultStyle}>
-      {props.children}
-      <button onClick={props.onToggle} className="cursor-pointer">
-        <svg
-          className="w-2 h-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 14">
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-          />
-        </svg>
-      </button>
-    </Badge>
-  );
-};
-
-const Indicator = ({ dir = "right", ...props }: Props) => {
-  const defaultStyle = cn(
-    `absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 ${
-      dir === "left" ? "-start-3 " : dir === "right" ? "-end-3 " : ""
-    } dark:border-gray-900`,
-    props.className
-  );
-
-  return (
-    <Badge className={defaultStyle} {...props}>
-      {props.children}
-    </Badge>
-  );
-};
+    );
+  }
+);
 
 export default Badge;
