2d1abd2793c160e5f1d3ab086d3cd53e6eebb5ef
criztiandev <criztiandev@gmail.com>
2024-02-25 16:57:08 +0800

    Finished Avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import Alert from "./components/ui/Alert";
+import Avatar from "./components/ui/Avatar";
 
 function App() {
   return (
     <div className="grid grid-cols-3 gap-4">
-      {/* <DisplayBlock title="Size"></DisplayBlock> */}
-      <Alert>HI</Alert>
-      <Alert variant="warning" border>
-        HI
-      </Alert>
-      <Alert variant="success">HI</Alert>
-      <Alert variant="danger">HI</Alert>
-      <Alert variant="info">HI</Alert>
+      <Avatar
+        src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        rounded
+      />
+
+      <Avatar src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
     </div>
   );
 }
--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,57 +1,33 @@
+import { ImgHTMLAttributes, forwardRef } from "react";
 import { cn } from "../../utils/tailwind.utils";
 
-interface Props {
-  src?: string;
-  className?: string;
+interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   rounded?: boolean;
-  indicator?: boolean;
-  dir?: "left" | "right";
-  color?: string;
+  border?: boolean;
+  fallback?: string;
 }
 
-const Avatar = (props: Props) => {
-  const defaultStyle = cn(
-    `relative w-10 h-10 overflow-hidden bg-gray-100  dark:bg-gray-600 
-     ${props.rounded ? "rounded-full" : "rounded-[8px]"}`,
-    props.className
-  );
+const Avatar = forwardRef<HTMLImageElement, Props>(
+  ({ className, ...props }, ref) => {
+    const defaultStyle = cn(
+      `relative flex h-10 w-10 shrink-0 overflow-hidden
+      ${props.rounded ? "rounded-full" : "rounded-[5px]"}
+    `,
+      className
+    );
 
-  const dotDefaultStyle = cn(
-    `top-0 left-7 absolute  w-3.5 h-3.5 border-2 border-white dark:border-gray-800 rounded-full
-        ${
-          props.dir === "left"
-            ? "-left-2 "
-            : props.dir === "right"
-            ? "-right-2 "
-            : ""
-        }
-        ${props.color ? `bg-[${props.color}]` : "bg-red-500"}
-    `
-  );
-
-  return (
-    <div className={defaultStyle}>
-      {props.indicator && <span className={dotDefaultStyle}></span>}
-      {props.src ? (
-        <img
-          className="w-10 h-10 rounded-full"
-          src="/docs/images/people/profile-picture-5.jpg"
-          alt="Rounded avatar"
-        />
-      ) : (
-        <svg
-          className="absolute w-12 h-12 text-gray-400 -left-1"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-            clipRule="evenodd"></path>
-        </svg>
-      )}
-    </div>
-  );
-};
+    return (
+      <div ref={ref} className={defaultStyle}>
+        {props.src ? (
+          <img {...props} />
+        ) : (
+          <span className="font-medium text-gray-600 dark:text-gray-300">
+            {props.fallback}
+          </span>
+        )}
+      </div>
+    );
+  }
+);
 
 export default Avatar;
